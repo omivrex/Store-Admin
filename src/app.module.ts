e32844e64920bf96b15a6b2a3c10cd436640c36a
RexOmiv <omivrex@gmail.com>
2022-08-29 05:38:53 +0100

    integrated postgres and typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      autoLoadEntities: true,
+      entities: []
+    }),
+    UsersModule,
+  ],
   controllers: [AppController, AuthController, ProductsController],
   providers: [AppService, AuthService, ProductsService],
 })
